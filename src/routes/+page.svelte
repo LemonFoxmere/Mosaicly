8d497b45fd9f8dc298b5b2eedc119ac5119d5558
Lemon Foxmere <lemon@lemonfoxmere.com>
2025-04-15 01:12:47 -0700

    setted up basic styling guidelines

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,57 @@
 <script lang="ts">
-	let x = $state(5);
-	let y = $derived(2 * x);
-	let z = $state(1);
-
-	$effect(() => {
-		z = x;
-	});
-
-	const testingFunc = () => {
-		x += 1;
-	};
 </script>
 
 <main>
-	<h1>{x}</h1>
-	<h2>{y}</h2>
-	<button onclick={testingFunc}>Add</button>
+	<section id="testing-box" class="item-frame">
+		<h1>This is a heading text.</h1>
+		<h2>This is a subheading</h2>
 
-	{#each new Array(z) as _}
-		<p>Test</p>
-	{/each}
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem consectetur
+			error, maxime minima accusamus amet ex voluptas, similique reprehenderit architecto
+			neque. Voluptates expedita dolores quibusdam cupiditate dolore exercitationem totam
+			quam. Aliquid asperiores quod culpa qui, itaque sint mollitia amet perferendis eveniet
+			quis maxime nihil iste, omnis assumenda architecto consectetur ea, est facere odit! Qui
+			assumenda odio harum eum hic excepturi!
+		</p>
+
+		<p class="caption">This is a caption. It's quite small</p>
+
+		<section id="btn-cluster">
+			<button>Solid Button</button>
+			<button class="solid-small">Solid Small</button>
+			<button class="outline">Outline Button</button>
+			<button class="outline-small">Outline Small</button>
+		</section>
+	</section>
 </main>
 
 <style lang="scss">
+	@use "$static/stylesheets/guideline" as *;
+
+	main {
+		width: 100%;
+		height: 100%;
+		padding: 30px;
+		display: flex;
+		justify-content: center;
+		align-items: flex-start;
+
+		#testing-box {
+			width: min(100%, 1000px);
+			padding: 40px;
+
+			display: flex;
+			flex-direction: column;
+			row-gap: 20px;
+
+			#btn-cluster {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				flex-wrap: wrap;
+				gap: 10px;
+			}
+		}
+	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,39 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+
+		env: {
+			dir: "./"
+		},
+
+		// route alias
+		alias: {
+			$static: "./static",
+			$comp: "$lib/comp",
+			$route: "src/routes"
+		}
+	},
+
+	vitePlugin: {
+		onwarn: (warning, handler) => {
+			// Suppress unused CSS selector warnings and accessibility warnings
+			if (warning.code === "css-unused-selector" || warning.code.startsWith("a11y-")) {
+				return;
+			}
+			handler(warning);
+		}
+	}
 };
 
 export default config;
