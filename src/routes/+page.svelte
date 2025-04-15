a128ce71e4b9ba148a897fd6387469e0d2d1fdb4
Lemon Foxmere <lemon@lemonfoxmere.com>
2025-04-14 22:46:29 -0700

    basic vite config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let x = $state(5);
+	let y = $derived(2 * x);
+	let z = $state(1);
+
+	$effect(() => {
+		z = x;
+	});
+
+	const testingFunc = () => {
+		x += 1;
+	};
+</script>
+
+<main>
+	<h1>{x}</h1>
+	<h2>{y}</h2>
+	<button onclick={testingFunc}>Add</button>
+
+	{#each new Array(z) as _}
+		<p>Test</p>
+	{/each}
+</main>
+
+<style lang="scss">
+</style>
