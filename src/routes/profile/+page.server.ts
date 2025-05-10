c53579b7914c0eb3edb6e76da190a6517c8aced6
Lemon Foxmere <lemon@lemonfoxmere.com>
2025-05-10 16:48:34 -0700

    minor changes

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,12 +1,14 @@
-import type { Actions } from "./$types";
 import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals: { supabase, user, safeGetSession } }) => {
 	const { session } = await safeGetSession();
 	if (!session) redirect(403, "/login");
 
 	const { data, error } = await supabase.from("canvas").select().eq("user_id", user.id);
+	void error;
+	// TODO: preprocess the data first
+
 	return { canvas: data };
 };
 
@@ -14,12 +16,12 @@ export const actions = {
 	// save profile on focus lost
 	update_profile: async ({ request, locals: { user, supabase } }) => {
 		const data = await request.formData();
-		const display_name = data.get("disp-name");
+		const displayName = data.get("disp-name");
 		const bio = data.get("bio");
 
 		const { error } = await supabase
 			.from("user_profile")
-			.update({ display_name, bio })
+			.update({ display_name: displayName, bio })
 			.eq("id", user.id);
 		if (error) {
 			return { success: false, message: `error: ${error}` };
