750fbf845f3e8287a002faff483181ca84e8c4c2
Beepered <bktrieu@gmail.com>
2025-05-23 13:35:49 -0700

    better qr page
    I hope this works

--- src/routes/qr-code/+page.svelte
+++ src/routes/qr-code/+page.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
-	import Qr from "$lib/comp/qrcode/qr.svelte";
+	import QrGenerator from "$lib/comp/qrcode/QrGenerator.svelte";
+
+	let inputText = $state("");
+	let finalText = $state(inputText);
 </script>
 
-<p>Qr code page</p>
-<Qr></Qr>
+<main>
+	<h1>QR code generator testing</h1>
+	<div>
+		<input type="text" bind:value={inputText} placeholder="canvas code" />
+		<button
+			onclick={() => {
+				finalText = inputText;
+			}}>Submit</button
+		>
+	</div>
+	<div id="QR-holder">
+		<QrGenerator QrSRC={finalText}></QrGenerator>
+	</div>
+</main>
 
-<style></style>
+<style lang="scss">
+	@use "$static/stylesheets/guideline" as *;
+
+	#QR-holder {
+		background: #fff;
+		padding: 2px;
+		border-radius: 5px;
+	}
+</style>
