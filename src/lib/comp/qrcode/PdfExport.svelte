27b83e8ed84776ca958bec871d6b6577e5cf2f99
Beepered <bktrieu@gmail.com>
2025-05-28 17:22:18 -0700

    PDF better fonts and logo, move logos and fonts to folder
    Created logo folder and fonts folder in static

--- src/lib/comp/qrcode/PdfExport.svelte
+++ src/lib/comp/qrcode/PdfExport.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import QRCode from "qrcode";
 	import jsPDF from "jspdf";
+	import QRCode from "qrcode";
 
 	let { QrSRC = "" } = $props();
 
@@ -14,16 +14,25 @@
 		const pageWidth = doc.internal.pageSize.width;
 		const pageHeight = doc.internal.pageSize.height;
 
+		doc.addFont("fonts/Outfit-Bold.ttf", "Outfit-Bold", "normal");
+		doc.addFont("fonts/Outfit-Regular.ttf", "Outfit-Regular", "normal");
+
 		// directly setting coordiates of elements
-		doc.setFont("Helvetica", "bold")
+		doc.setFont("Outfit-Bold", "normal")
 			.setFontSize(30)
-			.text("Like pixel art and shitposting?", pageWidth / 2, 90, { align: "center" });
-		doc.setFont("Helvetica", "normal")
+			.text("Like pixel art and shitposting?", pageWidth / 2, 80, { align: "center" });
+		doc.setFont("Outfit-Regular", "normal")
 			.setFontSize(18)
 			.setTextColor(146, 146, 146)
-			.text("Scan the QR code to start drawing on the X canvas", pageWidth / 2, 120, {
-				align: "center"
-			});
+			.text(
+				"Scan the QR code to start drawing on the " + "*name of canvas*" + " canvas",
+				pageWidth / 2,
+				110,
+				{
+					align: "center",
+					maxWidth: pageWidth * (2 / 3)
+				}
+			);
 
 		// qr-code is smaller than image size
 		doc.addImage(
@@ -34,27 +43,30 @@
 			pageWidth / 2,
 			pageWidth / 2
 		);
-		doc.setFont("Helvetica", "normal")
+
+		doc.setFont("Outfit-Regular", "normal")
 			.setFontSize(18)
 			.text(
-				"Is the QR code broken? Go to mosaicly.io/search and type in the\nbackup code:",
+				"Is the QR code broken? Go to mosaicly.io/search and type in the backup code:",
 				pageWidth / 2,
 				380,
 				{
-					align: "center"
+					align: "center",
+					maxWidth: pageWidth * (2 / 3)
 				}
 			);
-		doc.setFont("Helvetica", "bold")
+		doc.setFont("Outfit-Bold", "normal")
 			.setFontSize(25)
 			.setTextColor(0, 0, 0)
-			.text("XXXX-XXXX-XXXX", pageWidth / 2, 430, {
+			.text(QrSRC, pageWidth / 2, 430, {
 				align: "center"
 			});
-		doc.setFont("Helvetica", "bold")
-			.setFontSize(20)
-			.text("[] mosaicly", pageWidth / 2, 520, {
-				align: "center"
-			});
+
+		// logo
+		const img = new Image(); // Create new img element
+		img.src = "logos/logo-light-text.png";
+		const imgWidth = 120;
+		doc.addImage(img, "png", pageWidth / 2 - imgWidth / 2, 540, imgWidth, 25);
 		doc.save("qrcode.pdf");
 	}
 </script>
