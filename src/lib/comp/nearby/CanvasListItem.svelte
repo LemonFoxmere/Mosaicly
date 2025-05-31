1ede9eda856b98f69a3e642af423595cc63f5e8a
smoke-me <shreyanshkaushik@gmail.com>
2025-05-31 01:04:38 -0700

    nearby canvases implemented

---
+++ src/lib/comp/nearby/CanvasListItem.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+	import CanvasCard from "$lib/comp/ui/listItems/CanvasCard.svelte";
+
+	interface Props {
+		title: string;
+		locDesc: string;
+		distance: string;
+		onClick?: () => void;
+		ctaOptions?: Snippet;
+	}
+	let { title, locDesc, distance, onClick, ctaOptions }: Props = $props();
+</script>
+
+<CanvasCard {title} {locDesc} {distance} clickable={true} {onClick} {ctaOptions} />
